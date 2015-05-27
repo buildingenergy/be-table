75f5e5735715416451810e9f5464bbabc251597d
Michael Dougherty <michael.dougherty@buildingenergy.com>
2015-05-27 15:48:35 -0700

    spaces

--- __tests__/be-table-test.js
+++ __tests__/be-table-test.js
@@ -9,26 +9,26 @@ var BE = require(srcFile);
 var TU = React.addons.TestUtils;
 
 var columns = [
-	{
-		key: 'city',
-		title: 'City',
-		sortable: true,
-		type: 'string',
-	},
-	{
-		key: 'gfa',
-		title: 'Gross Floor Area (ft2)',
-		sortable: true,
-		type: 'number',
-	},
+  {
+    key: 'city',
+    title: 'City',
+    sortable: true,
+    type: 'string',
+  },
+  {
+    key: 'gfa',
+    title: 'Gross Floor Area (ft2)',
+    sortable: true,
+    type: 'number',
+  },
 ];
 
 var rows = [
-	{'city': 'San Francisco', 'gfa': 1000},
-	{'city': 'San Francisco', 'gfa': 2000},
-	{'city': 'San Francisco', 'gfa': 3000},
-	{'city': 'Portland', 'gfa': 1000},
-	{'city': 'Portland', 'gfa': 2000},
+  {'city': 'San Francisco', 'gfa': 1000},
+  {'city': 'San Francisco', 'gfa': 2000},
+  {'city': 'San Francisco', 'gfa': 3000},
+  {'city': 'Portland', 'gfa': 1000},
+  {'city': 'Portland', 'gfa': 2000},
 ];
 
 var callback = function() {
@@ -36,16 +36,16 @@ var callback = function() {
 };
 
 describe('BETable', function () {
-	it('works', function () {
-		var table = TU.renderIntoDocument(
-			React.createElement(BE.BETable, {
-				columns: columns,
-	            rows: rows,
-	            searchmeta: {},
-	            callback: callback,
-	            // customTypes: {},
-	        })
-		);
-		expect(3).toBe(3);
-	});
+  it('works', function () {
+    var table = TU.renderIntoDocument(
+      React.createElement(BE.BETable, {
+        columns: columns,
+        rows: rows,
+        searchmeta: {},
+        callback: callback,
+        // customTypes: {},
+      })
+    );
+    expect(3).toBe(3);
+  });
 })
