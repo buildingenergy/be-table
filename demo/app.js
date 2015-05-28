408b81b035ef6153a37212c6b57f184f2bbee7ed
Aleck Landgraf <aleck.landgraf@buildingenergy.com>
2015-05-28 14:17:24 -0700

    demo

---
+++ demo/app.js
@@ -0,0 +1,36 @@
+/**
+* Demo of be-table
+*/
+
+
+angular.module('app', ['react'])
+.controller('beCtrl', ['$scope', '$log', function ($scope, $log) {
+  $scope.columns = [
+    {key: 'price', title: 'Price', subtitle: '$', type: 'number'},
+    {key: 'item', title: 'Item', type: 'string'},
+    {key: 'label', title: 'Label', type: 'label'},
+  ];
+  $scope.rows = [
+    {item: 'kale', price: 4.34, label: {color: 'green', text: 'Leafy'}},
+    {item: 'almonds', price: 5.44, label: {color: 'blue', text: 'Nutty'}},
+  ];
+  $scope.tableCallback = function (state) {console.log (state);};
+  $scope.paginationInfo = {totalMatchCount: 2000};
+  $scope.customTypes = {
+    label: {
+      cell: {
+        className: 'label',
+        renderer: function(value, row, col, state) {
+          return React.createElement(Label, {
+            color: value.color,
+          }, [value.name]);
+        }
+      },
+      header: { /* similar format to "cell" definition */ },
+    filter: { /* similar format to "cell" definition */ },
+    }
+  };
+}])
+.directive('betable', function(reactDirective) {
+  return reactDirective(window.BE.Table.BETable);
+});
