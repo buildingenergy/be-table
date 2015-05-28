a13c7c2b6c30f0f1e07bd4a56599dd577547e825
Aleck Landgraf <aleck.landgraf@buildingenergy.com>
2015-05-27 18:11:24 -0700

    merge, tests headers

---
+++ __tests__/header-test.js
@@ -0,0 +1,52 @@
+
+var srcFile = '../build/js/be-table';
+jest.dontMock(srcFile + '.js');
+jest.dontMock('lodash');
+
+var React = window.React = require('react/addons');
+var _ = window._ = require('lodash');
+var BE = require(srcFile);
+var TU = React.addons.TestUtils;
+
+var tableAttrs = {
+  columns: [
+    {
+      key: 'city',
+      title: 'City',
+      sortable: true,
+      type: 'string',
+    },
+    {
+      key: 'gfa',
+      title: 'Gross Floor Area (ft2)',
+      sortable: true,
+      type: 'number',
+    },
+  ],
+  rows: [
+    {'city': 'Portland', 'gfa': 1000},
+    {'city': 'Portland', 'gfa': 2000},
+    {'city': 'Seattle', 'gfa': 1000},
+    {'city': 'Seattle', 'gfa': 2000},
+    {'city': 'Seattle', 'gfa': 3000},
+  ],
+  callback: _.noop,
+  searchmeta: {},
+};
+
+var renderTable = function(attrs) {
+  return TU.renderIntoDocument(
+    React.createElement(BE.BETable, attrs)
+  );
+};
+
+describe('BETable', function () {
+  it('renders headers', function () {
+    var table = renderTable(tableAttrs);
+    var thead = TU.findRenderedDOMComponentWithTag(table, 'thead')
+    var headers = thead.props.children[0].props.children;
+    expect(headers[0].key).toBe('city');
+    expect(headers[1].key).toBe('gfa');
+  });
+
+})
