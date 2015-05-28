c9c0259b86422822bd62274d6db1995695b354b7
Aleck Landgraf <aleck.landgraf@buildingenergy.com>
2015-05-28 15:15:58 -0700

    working on sort callback

--- demo/app.js
+++ demo/app.js
@@ -4,20 +4,29 @@
 
 
 angular.module('app', ['react'])
-.controller('beCtrl', ['$scope', '$log', function ($scope, $log) {
+.controller('beCtrl', ['$scope', '$log', '$filter', function ($scope, $log, $filter) {
   $scope.columns = [
     {key: 'price', title: 'Price', subtitle: '$', type: 'number'},
     {key: 'item', title: 'Item', type: 'string'},
-    {key: 'label', title: 'Label', type: 'label'},
+    {key: 'label', title: 'Label', type: 'label'}
   ];
   $scope.rows = [
-    {item: 'kale', price: 4.34, label: {color: 'green', text: 'Leafy'}},
-    {item: 'almonds', price: 5.44, label: {color: 'blue', text: 'Nutty'}},
+    {item: 'kale', price: 4.34, label: {text: 'Leafy'}},
+    {item: 'almonds', price: 5.44, label: {text: 'Nutty'}},
+    {item: 'strawberries', price: 3.20, label: {text: 'Ripe'}}
   ];
-  $scope.tableCallback = function (state) {
-    $log.info(state);
+  $scope.tableCallback = function (state, tableEvent) {
+    $log.info(state, tableEvent);
+    if (tableEvent && tableEvent.eventType && tableEvent.eventType === 'columnSorted') {
+      $scope.rows = _.sortBy($scope.rows, state.sorting.column.key);
+    }
   };
-  $scope.paginationInfo = {totalMatchCount: 2000};
+  $scope.paginationInfo = {totalMatchCount: 3};
+
+
+  /**
+   * react custom renderer for the 'label' type
+   */
   $scope.customTypes = {
     label: {
       cell: {
